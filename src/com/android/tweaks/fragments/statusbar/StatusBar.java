8027e52950adbadbb3ac9ce9e696998aacac5744
Pranav Vashi <neobuddy89@gmail.com>
2024-05-05 17:54:24 +0000

    Statusbar: Status bar quick pull down
    Co-Authored-By: Simon Shields <simon@lineageos.org>
    Co-Authored-By: Michael Bestas <mkbestas@lineageos.org>
    Co-Authored-By: LuK1337 <priv.luk@gmail.com>
    Co-Authored-By: Luca Stefani <luca.stefani.ge1@gmail.com>
    Co-Authored-By: Bruno Martins <bgcngm@gmail.com>
    Co-Authored-By: Michael W <baddaemon87@gmail.com>

--- src/com/android/tweaks/fragments/statusbar/StatusBar.java
+++ src/com/android/tweaks/fragments/statusbar/StatusBar.java
@@ -9,6 +9,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
@@ -23,11 +24,31 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
+import lineageos.preference.LineageSystemSettingListPreference;
+
+import com.android.settings.preferences.SystemSettingSwitchPreference;
+import com.android.tweaks.utils.DeviceUtils;
+
 @SearchIndexable
 public class StatusBar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "StatusBar";
+
+    private static final String KEY_QUICK_PULLDOWN = "qs_quick_pulldown";
+
+    private static final String KEY_ICONS_CATEGORY = "status_bar_icons_category";
+    private static final String KEY_BLUETOOTH_BATTERY_STATUS = "bluetooth_show_battery";
+
+    private static final int PULLDOWN_DIR_NONE = 0;
+    private static final int PULLDOWN_DIR_RIGHT = 1;
+    private static final int PULLDOWN_DIR_LEFT = 2;
+    private static final int PULLDOWN_DIR_BOTH = 3;
+
+    private LineageSystemSettingListPreference mQuickPulldown;
+
+    private PreferenceCategory mIconsCategory;
+    private SystemSettingSwitchPreference mBluetoothBatteryStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,13 +59,60 @@ public class StatusBar extends SettingsPreferenceFragment implements
         final ContentResolver resolver = context.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources resources = context.getResources();
+
+        mQuickPulldown =
+                (LineageSystemSettingListPreference) findPreference(KEY_QUICK_PULLDOWN);
+        mQuickPulldown.setOnPreferenceChangeListener(this);
+        updateQuickPulldownSummary(mQuickPulldown.getIntValue(0));
+
+        mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
+        mBluetoothBatteryStatus = (SystemSettingSwitchPreference) findPreference(KEY_BLUETOOTH_BATTERY_STATUS);
+
+        if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+            mQuickPulldown.setEntries(R.array.status_bar_quick_pull_down_entries_rtl);
+            mQuickPulldown.setEntryValues(R.array.status_bar_quick_pull_down_values_rtl);
+        }
+
+        if (!DeviceUtils.deviceSupportsBluetooth(context)) {
+            mIconsCategory.removePreference(mBluetoothBatteryStatus);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
+        if (preference == mQuickPulldown) {
+            int value = Integer.parseInt((String) newValue);
+            updateQuickPulldownSummary(value);
+            return true;
+        }
         return false;
+    }
+
+    private void updateQuickPulldownSummary(int value) {
+        String summary = "";
+        switch (value) {
+            case PULLDOWN_DIR_NONE:
+                summary = getResources().getString(
+                    R.string.status_bar_quick_pull_down_off);
+                break;
+            case PULLDOWN_DIR_RIGHT:
+            case PULLDOWN_DIR_LEFT:
+            case PULLDOWN_DIR_BOTH:
+                summary = getResources().getString(
+                    R.string.status_bar_quick_pull_down_summary,
+                    getResources().getString(
+                        value == PULLDOWN_DIR_RIGHT
+                            ? R.string.status_bar_quick_pull_down_right
+                            : value == PULLDOWN_DIR_LEFT
+                                ? R.string.status_bar_quick_pull_down_left
+                                : R.string.status_bar_quick_pull_down_both
+                    )
+                );
+                break;
+        }
+        mQuickPulldown.setSummary(summary);
     }
 
     @Override
