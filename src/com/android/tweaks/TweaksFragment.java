5fef8ad94fe9d8715be6baf649cfa56b79da9f1e
LouisG1977 <luisgonzales272@gmail.com>
2025-01-23 11:33:04 -0600

    Settings: Revamp UI
    Signed-off-by: Louis1977 <luisgonzales272@gmail.com>
    Co-Authored-By: jhonboy121 <alfredmathew05@gmail.com>
    Co-Authored-By: Ido Ben-Hur <idoybh2@gmail.com>

--- src/com/android/tweaks/TweaksFragment.java
+++ src/com/android/tweaks/TweaksFragment.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,11 +18,6 @@ package com.android.tweaks;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
@@ -30,83 +25,27 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TweaksFragment extends DashboardFragment {
-
+ public class TweaksFragment extends SettingsPreferenceFragment {
+ 
     public static final String CATEGORY_KEY = "com.android.settings.category.ia.tweaks";
+
     private static final String LOG_TAG = "Tweaks";
 
-    @Override
-    protected int getPreferenceScreenResId() {
-        return R.xml.tweaks;
-    }
+     @Override
+     public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+ 
+         addPreferencesFromResource(R.xml.xtended_dashboard);
+     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.VIEW_UNKNOWN;
     }
-
-    @Override
-    public int getHelpResource() {
-        return R.string.help_uri_about;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected String getLogTag() {
-        return LOG_TAG;
-    }
-
-    @Override
-    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
-        return buildPreferenceControllers(context, this /* fragment */, getSettingsLifecycle());
-    }
-
-    private static List<AbstractPreferenceController> buildPreferenceControllers(
-            Context context, TweaksFragment fragment, Lifecycle lifecycle) {
-        final List<AbstractPreferenceController> controllers = new ArrayList<>();
-        controllers.add(new TweaksController(context));
-        return controllers;
-    }
-
-    @Override
-    public RecyclerView onCreateRecyclerView(LayoutInflater inflater, ViewGroup container, Bundle icicle) {
-        RecyclerView rcv = super.onCreateRecyclerView(inflater, container, icicle);
-        GridLayoutManager layoutG = new GridLayoutManager(getActivity(), 2);
-        layoutG.setSpanSizeLookup(new SpanSizeLookupG());
-        rcv.setLayoutManager(layoutG);
-        return rcv;
-    }
-
-    class SpanSizeLookupG extends GridLayoutManager.SpanSizeLookup {
-        @Override
-        public int getSpanSize(int position) {
-            if (position == 0 || position == 1) {
-                return 2;
-            } else {
-                return 1;
-            }
-        }
-    }
-
-    /**
-     * For Search.
-     */
-    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.tweaks) {
-                @Override
-                public List<AbstractPreferenceController> createPreferenceControllers(
-                        Context context) {
-                    return buildPreferenceControllers(context, null /* fragment */,
-                            null /* lifecycle */);
-                }
-            };
 }
