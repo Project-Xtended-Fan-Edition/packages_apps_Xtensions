crazy-stuffs/packages_apps_BaseCamp

Power Menu: Power menu actions
Signed-off-by: AnierinB <anierin@evolution-x.org>

src/com/everest/basecamp/fragments/powermenu/PowerMenuActions.java
@@ -1,0 +1,223 @@
+/*
+ * Copyright (C) 2016-2024 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.everest.basecamp.fragments.powermenu;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.UserInfo;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.provider.Settings;
+import android.service.controls.ControlsProviderService;
+
+import androidx.preference.SwitchPreferenceCompat;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+
+import org.lineageos.internal.util.PowerMenuConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.widget.LockPatternUtils;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.applications.ServiceListing;
+
+import com.android.settings.R;
+
+import com.everest.basecamp.utils.TelephonyUtils;
+
+import lineageos.app.LineageGlobalActions;
+import lineageos.providers.LineageSettings;
+
+import static org.lineageos.internal.util.PowerMenuConstants.*;
+
+public class PowerMenuActions extends SettingsPreferenceFragment {
+    final static String TAG = "PowerMenuActions";
+
+    private static final String CATEGORY_POWER_MENU_ITEMS = "power_menu_items";
+
+    private SwitchPreferenceCompat mScreenshotPref;
+    private SwitchPreferenceCompat mOnTheGoPref;
+    private SwitchPreferenceCompat mAirplanePref;
+    private SwitchPreferenceCompat mUsersPref;
+    private SwitchPreferenceCompat mLockDownPref;
+    private SwitchPreferenceCompat mEmergencyPref;
+    private SwitchPreferenceCompat mDeviceControlsPref;
+
+    private LineageGlobalActions mLineageGlobalActions;
+
+    Context mContext;
+    private LockPatternUtils mLockPatternUtils;
+    private UserManager mUserManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.power_menu_actions);
+        mContext = getActivity().getApplicationContext();
+        mLockPatternUtils = new LockPatternUtils(mContext);
+        mUserManager = UserManager.get(mContext);
+        mLineageGlobalActions = LineageGlobalActions.getInstance(mContext);
+
+        for (String action : PowerMenuConstants.getAllActions()) {
+            if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
+                mScreenshotPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
+            } else if (action.equals(GLOBAL_ACTION_KEY_ONTHEGO)) {
+                mOnTheGoPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_ONTHEGO);
+            } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
+                mAirplanePref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
+            } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
+                mUsersPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_USERS);
+            } else if (action.equals(GLOBAL_ACTION_KEY_LOCKDOWN)) {
+                mLockDownPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
+            } else if (action.equals(GLOBAL_ACTION_KEY_EMERGENCY)) {
+                mEmergencyPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
+            } else if (action.equals(GLOBAL_ACTION_KEY_DEVICECONTROLS)) {
+                mDeviceControlsPref = findPreference(GLOBAL_ACTION_KEY_DEVICECONTROLS);
+            }
+        }
+
+        if (!TelephonyUtils.isVoiceCapable(getActivity())) {
+            getPreferenceScreen().removePreference(mEmergencyPref);
+            mEmergencyPref = null;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (mScreenshotPref != null) {
+            mScreenshotPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_SCREENSHOT));
+        }
+
+        if (mOnTheGoPref != null) {
+            mOnTheGoPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_ONTHEGO));
+        }
+
+        if (mAirplanePref != null) {
+            mAirplanePref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_AIRPLANE));
+        }
+
+        if (mEmergencyPref != null) {
+            mEmergencyPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_EMERGENCY));
+        }
+
+        if (mDeviceControlsPref != null) {
+            mDeviceControlsPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_DEVICECONTROLS));
+
+            // Enable preference if any device control app is installed
+            ServiceListing serviceListing = new ServiceListing.Builder(mContext)
+                    .setIntentAction(ControlsProviderService.SERVICE_CONTROLS)
+                    .setPermission(Manifest.permission.BIND_CONTROLS)
+                    .setNoun("Controls Provider")
+                    .setSetting("controls_providers")
+                    .setTag("controls_providers")
+                    .build();
+            serviceListing.addCallback(
+                    services -> mDeviceControlsPref.setEnabled(!services.isEmpty()));
+            serviceListing.reload();
+        }
+
+        updatePreferences();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updatePreferences();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        boolean value;
+
+        if (preference == mScreenshotPref) {
+            value = mScreenshotPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENSHOT);
+
+        } else if (preference == mOnTheGoPref) {
+            value = mOnTheGoPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_ONTHEGO);
+
+        } else if (preference == mAirplanePref) {
+            value = mAirplanePref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_AIRPLANE);
+
+        } else if (preference == mUsersPref) {
+            value = mUsersPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_USERS);
+
+        } else if (preference == mLockDownPref) {
+            value = mLockDownPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_LOCKDOWN);
+
+        } else if (preference == mEmergencyPref) {
+            value = mEmergencyPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_EMERGENCY);
+
+        } else if (preference == mDeviceControlsPref) {
+            value = mDeviceControlsPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_DEVICECONTROLS);
+
+        } else {
+            return super.onPreferenceTreeClick(preference);
+        }
+        return true;
+    }
+
+    private void updatePreferences() {
+        boolean isKeyguardSecure = mLockPatternUtils.isSecure(UserHandle.myUserId());
+        if (mLockDownPref != null) {
+            mLockDownPref.setEnabled(isKeyguardSecure);
+            mLockDownPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_LOCKDOWN));
+            if (isKeyguardSecure) {
+                mLockDownPref.setSummary(null);
+            } else {
+                mLockDownPref.setSummary(R.string.power_menu_actions_lockdown_unavailable);
+            }
+        }
+        if (mUsersPref != null) {
+            if (!UserHandle.MU_ENABLED || !UserManager.supportsMultipleUsers()) {
+                getPreferenceScreen().removePreference(mUsersPref);
+                mUsersPref = null;
+            } else {
+                List<UserInfo> users = mUserManager.getUsers();
+                boolean enabled = (users.size() > 1);
+                mUsersPref.setChecked(mLineageGlobalActions.userConfigContains(
+                        GLOBAL_ACTION_KEY_USERS) && enabled);
+                mUsersPref.setEnabled(enabled);
+            }
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVEREST;
+    }
+}

Project-Xtended-Fan-Edition/packages_apps_Xtensions

Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/tweaks/fragments/powermenu/PowerMenuActions.java
@@ -1,0 +1,223 @@
+/*
+ * Copyright (C) 2016-2024 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tweaks.fragments.powermenu;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.UserInfo;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.provider.Settings;
+import android.service.controls.ControlsProviderService;
+
+import androidx.preference.SwitchPreferenceCompat;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+
+import org.lineageos.internal.util.PowerMenuConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.widget.LockPatternUtils;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.applications.ServiceListing;
+
+import com.android.settings.R;
+
+import com.android.tweaks.utils.TelephonyUtils;
+
+import lineageos.app.LineageGlobalActions;
+import lineageos.providers.LineageSettings;
+
+import static org.lineageos.internal.util.PowerMenuConstants.*;
+
+public class PowerMenuActions extends SettingsPreferenceFragment {
+    final static String TAG = "PowerMenuActions";
+
+    private static final String CATEGORY_POWER_MENU_ITEMS = "power_menu_items";
+
+    private SwitchPreferenceCompat mScreenshotPref;
+    private SwitchPreferenceCompat mOnTheGoPref;
+    private SwitchPreferenceCompat mAirplanePref;
+    private SwitchPreferenceCompat mUsersPref;
+    private SwitchPreferenceCompat mLockDownPref;
+    private SwitchPreferenceCompat mEmergencyPref;
+    private SwitchPreferenceCompat mDeviceControlsPref;
+
+    private LineageGlobalActions mLineageGlobalActions;
+
+    Context mContext;
+    private LockPatternUtils mLockPatternUtils;
+    private UserManager mUserManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.power_menu_actions);
+        mContext = getActivity().getApplicationContext();
+        mLockPatternUtils = new LockPatternUtils(mContext);
+        mUserManager = UserManager.get(mContext);
+        mLineageGlobalActions = LineageGlobalActions.getInstance(mContext);
+
+        for (String action : PowerMenuConstants.getAllActions()) {
+            if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
+                mScreenshotPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
+            } else if (action.equals(GLOBAL_ACTION_KEY_ONTHEGO)) {
+                mOnTheGoPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_ONTHEGO);
+            } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
+                mAirplanePref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
+            } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
+                mUsersPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_USERS);
+            } else if (action.equals(GLOBAL_ACTION_KEY_LOCKDOWN)) {
+                mLockDownPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
+            } else if (action.equals(GLOBAL_ACTION_KEY_EMERGENCY)) {
+                mEmergencyPref = (SwitchPreferenceCompat) findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
+            } else if (action.equals(GLOBAL_ACTION_KEY_DEVICECONTROLS)) {
+                mDeviceControlsPref = findPreference(GLOBAL_ACTION_KEY_DEVICECONTROLS);
+            }
+        }
+
+        if (!TelephonyUtils.isVoiceCapable(getActivity())) {
+            getPreferenceScreen().removePreference(mEmergencyPref);
+            mEmergencyPref = null;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (mScreenshotPref != null) {
+            mScreenshotPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_SCREENSHOT));
+        }
+
+        if (mOnTheGoPref != null) {
+            mOnTheGoPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_ONTHEGO));
+        }
+
+        if (mAirplanePref != null) {
+            mAirplanePref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_AIRPLANE));
+        }
+
+        if (mEmergencyPref != null) {
+            mEmergencyPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_EMERGENCY));
+        }
+
+        if (mDeviceControlsPref != null) {
+            mDeviceControlsPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_DEVICECONTROLS));
+
+            // Enable preference if any device control app is installed
+            ServiceListing serviceListing = new ServiceListing.Builder(mContext)
+                    .setIntentAction(ControlsProviderService.SERVICE_CONTROLS)
+                    .setPermission(Manifest.permission.BIND_CONTROLS)
+                    .setNoun("Controls Provider")
+                    .setSetting("controls_providers")
+                    .setTag("controls_providers")
+                    .build();
+            serviceListing.addCallback(
+                    services -> mDeviceControlsPref.setEnabled(!services.isEmpty()));
+            serviceListing.reload();
+        }
+
+        updatePreferences();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updatePreferences();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        boolean value;
+
+        if (preference == mScreenshotPref) {
+            value = mScreenshotPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENSHOT);
+
+        } else if (preference == mOnTheGoPref) {
+            value = mOnTheGoPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_ONTHEGO);
+
+        } else if (preference == mAirplanePref) {
+            value = mAirplanePref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_AIRPLANE);
+
+        } else if (preference == mUsersPref) {
+            value = mUsersPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_USERS);
+
+        } else if (preference == mLockDownPref) {
+            value = mLockDownPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_LOCKDOWN);
+
+        } else if (preference == mEmergencyPref) {
+            value = mEmergencyPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_EMERGENCY);
+
+        } else if (preference == mDeviceControlsPref) {
+            value = mDeviceControlsPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_DEVICECONTROLS);
+
+        } else {
+            return super.onPreferenceTreeClick(preference);
+        }
+        return true;
+    }
+
+    private void updatePreferences() {
+        boolean isKeyguardSecure = mLockPatternUtils.isSecure(UserHandle.myUserId());
+        if (mLockDownPref != null) {
+            mLockDownPref.setEnabled(isKeyguardSecure);
+            mLockDownPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_LOCKDOWN));
+            if (isKeyguardSecure) {
+                mLockDownPref.setSummary(null);
+            } else {
+                mLockDownPref.setSummary(R.string.power_menu_actions_lockdown_unavailable);
+            }
+        }
+        if (mUsersPref != null) {
+            if (!UserHandle.MU_ENABLED || !UserManager.supportsMultipleUsers()) {
+                getPreferenceScreen().removePreference(mUsersPref);
+                mUsersPref = null;
+            } else {
+                List<UserInfo> users = mUserManager.getUsers();
+                boolean enabled = (users.size() > 1);
+                mUsersPref.setChecked(mLineageGlobalActions.userConfigContains(
+                        GLOBAL_ACTION_KEY_USERS) && enabled);
+                mUsersPref.setEnabled(enabled);
+            }
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVEREST;
+    }
+}