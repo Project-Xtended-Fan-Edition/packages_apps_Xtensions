LouisG1977/android_packages_apps_Personalizations

Max Lockscreen Notification count [2/2]
Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
Signed-off-by: mukesh22584 <mks22584@gmail.com>
Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
Co-Authored-By: beanstown106 <nbenis106@gmail.com>
Co-Authored-By: SuperDroidBond <superdroidbond@yahoo.com>

src/com/crdroid/settings/fragments/LockScreen.java
@@ -42,6 +42,7 @@ import com.android.settingslib.search.SearchIndexable;
 
 import com.crdroid.settings.fragments.lockscreen.UdfpsAnimation;
 import com.crdroid.settings.fragments.lockscreen.UdfpsIconPicker;
+import com.crdroid.settings.preferences.CustomSeekBarPreference;
 
 import java.util.List;
 
@@ -67,6 +68,7 @@ public class LockScreen extends SettingsPreferenceFragment
     private static final String KEY_UDFPS_ICONS = "udfps_icon_picker";
     private static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
     private static final String KEY_KG_USER_SWITCHER= "kg_user_switcher_enabled";
+    private static final String LOCKSCREEN_MAX_NOTIF_CONFIG = "lockscreen_max_notif_cofig";
 
     private Preference mUdfpsAnimations;
     private Preference mUdfpsIcons;
@@ -74,7 +76,8 @@ public class LockScreen extends SettingsPreferenceFragment
     private Preference mWeather;
     private Preference mScreenOffUdfps;
     private Preference mUserSwitcher;
-    
+    private CustomSeekBarPreference mMaxKeyguardNotifConfig;  
+
     private OmniJawsClient mWeatherClient;
 
     @Override
@@ -120,6 +123,12 @@ public class LockScreen extends SettingsPreferenceFragment
         mWeatherClient = new OmniJawsClient(getContext());
         updateWeatherSettings();
         
+        mMaxKeyguardNotifConfig = (CustomSeekBarPreference) findPreference(LOCKSCREEN_MAX_NOTIF_CONFIG);
+        int kgconf = Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 3);
+        mMaxKeyguardNotifConfig.setValue(kgconf);
+        mMaxKeyguardNotifConfig.setOnPreferenceChangeListener(this);
+
         PreferenceScreen screen = getPreferenceScreen();
         PreferenceUtils.hideEmptyCategory(gestCategory, screen);
         PreferenceUtils.hideEmptyCategory(udfpsCategory, screen);
@@ -131,8 +140,21 @@ public class LockScreen extends SettingsPreferenceFragment
         if (preference == mUserSwitcher) {
             SystemRestartUtils.showSystemUIRestartDialog(getContext());
             return true;
+        } else if (preference == mMaxKeyguardNotifConfig) {
+            int kgconf = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, kgconf);
+            return true;
         }
         return false;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCK_SCREEN_CUSTOM_NOTIF, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
     }
 
     private void updateWeatherSettings() {

Project-Xtended-Fan-Edition/packages_apps_Xtensions

src/com/android/tweaks/fragments/lockscreen/LockScreen.java
@@ -10,6 +10,8 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
@@ -21,6 +23,8 @@ import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.android.settings.preferences.SystemSettingSeekBarPreference;
 
 import java.util.List;
 
@@ -34,7 +38,9 @@ public class LockScreen extends SettingsPreferenceFragment implements
     private static final String KEY_RIPPLE_EFFECT = "enable_ripple_effect";
     private static final String KEY_AUTHENTICATION_SUCCESS = "fp_success_vibrate";
     private static final String KEY_AUTHENTICATION_ERROR = "fp_error_vibrate";
+    private static final String LOCKSCREEN_MAX_NOTIF_CONFIG = "lockscreen_max_notif_cofig";
 
+    private SystemSettingSeekBarPreference mMaxKeyguardNotifConfig;  
     private PreferenceCategory mFingerprintCategory;
 
     @Override
@@ -46,6 +52,12 @@ public class LockScreen extends SettingsPreferenceFragment implements
         final ContentResolver resolver = context.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources resources = context.getResources();
+
+        mMaxKeyguardNotifConfig = (SystemSettingSeekBarPreference) findPreference(LOCKSCREEN_MAX_NOTIF_CONFIG);
+        int kgconf = Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 3);
+        mMaxKeyguardNotifConfig.setValue(kgconf);
+        mMaxKeyguardNotifConfig.setOnPreferenceChangeListener(this);
 
         mFingerprintCategory = (PreferenceCategory) findPreference(KEY_FINGERPRINT_CATEGORY);
 
@@ -61,7 +73,21 @@ public class LockScreen extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
+      if (preference == mMaxKeyguardNotifConfig) {
+            int kgconf = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, kgconf);
+            return true;
+        }
         return false;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCK_SCREEN_CUSTOM_NOTIF, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
     }
 
     @Override